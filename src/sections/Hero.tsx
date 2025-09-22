8cf3daf75c9f1903b5ed3d438d33906af3d8d0ac
Rohit-998 <rohitagrawal99819@gmail.com>
2025-09-22 21:47:24 +0530

    fixed res

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,9 +1,8 @@
-import { motion } from "motion/react";
+import { motion } from "framer-motion";
 
 export function Hero() {
   return (
     <section className="relative mx-auto max-w-5xl px-6 pt-20 pb-12 text-center">
-   
       <motion.h1
         className="text-4xl sm:text-5xl font-extrabold tracking-tight flex items-center justify-center gap-3"
         initial={{ opacity: 0, y: 20 }}
@@ -13,20 +12,26 @@ export function Hero() {
         <img
           src="/gdg.svg"
           alt="Google Developer Groups"
-          className="h-10 sm:h-12 w-auto"
+          className="h-10 sm:h-12 w-auto dark:hidden"
+        />
+
+        <img
+          src="/gdgwhite.svg"
+          alt="Google Developer Groups (Dark)"
+          className="h-10 sm:h-12 w-auto hidden dark:block"
         />
       </motion.h1>
 
-      <h2 className="text-[#646464] font-bold">RBU Chapter</h2>
+      <h2 className="text-[#646464] dark:text-gray-300 font-bold">
+        RBU Chapter
+      </h2>
 
-    
       <img
         src="/i1.svg"
         alt="Students illustration"
         className="mx-auto mt-10 w-full max-w-md"
       />
 
- 
       <motion.p
         className="mt-4 text-gray-600 dark:text-gray-300 max-w-2xl font-pt-sans-400 text-[25px] leading-relaxed mx-auto"
         initial={{ opacity: 0, y: 10 }}
@@ -40,16 +45,12 @@ export function Hero() {
       <motion.button
         whileHover={{ y: -2 }}
         whileTap={{ scale: 0.98 }}
-        className="mt-6 inline-flex items-center justify-center rounded-md  py-3 text-white shadow-sm font-open-sans-700 bg-[#3686F7] px-11 "
+        className="mt-6 inline-flex items-center justify-center rounded-md py-3 px-11 text-white shadow-sm font-open-sans-700 bg-[#3686F7]"
       >
         Join Us
       </motion.button>
 
-     
       <img src="/downArrow.svg" alt="" className="mx-auto mt-6 mb-27" />
-
-     
-  
     </section>
   );
 }
